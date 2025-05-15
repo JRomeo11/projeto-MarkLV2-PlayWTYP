b1124ea9d58143ba6b7122989137ce732a594083
JRomeo11 <jefersonntx@gmail.com>
2025-05-14 22:10:35 -0300

    Reutilização de massa de testes e API

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test'
+import { faker } from '@faker-js/faker';
 
-test('deve poder cadastrar uma nova tarefa', async ({ page }) => {
+test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
+
+    const objfs = 'Estudar automação'
+
+    await request.delete('http://localhost:3333/helper/tasks/' + objfs)
 
     await page.goto('http://localhost:8080')
 
     const inputTaskName = page.locator('input[placeholder="Add a new Task"]')
-    await inputTaskName.fill('Esse é  um teste para escrever uma task')
+    //await inputTaskName.fill(faker.music.songName())
+    await inputTaskName.fill(objfs)
 
     await page.click('xpath=//button[contains(text(), "Create")]')
     // await page.click('css=button >> text=Create')    
